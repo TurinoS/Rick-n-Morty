04cd7edc343caa0549dd46a8bb65a931f3f8c0fe
Paulo Turino <paulo.turino01@gmail.com>
2023-11-29 10:59:15 -0300

    feat: Create search input component

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -3,11 +3,12 @@ import { useContext } from "react";
 import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
 import { ContextAPI } from "../context/ContextAPI";
 import Button from "./Button";
+import SearchInput from "./SearchInput";
 
 export default function Pagination() {
   const { page, setPage } = useContext(ContextAPI);
   return (
-    <div className="flex gap-5 items-center">
+    <div className="flex gap-5 items-center relative ml-16">
       <Button page={page} onClick={() => (page > 1 ? setPage(page - 1) : {})}>
         <MdArrowBackIos /> Prev
       </Button>
@@ -15,6 +16,7 @@ export default function Pagination() {
       <Button page={page} onClick={() => (page < 42 ? setPage(page + 1) : {})}>
         Next <MdArrowForwardIos />
       </Button>
+      <SearchInput />
     </div>
   );
 }
